09d22d0c41b6c7d2b0adc28e6bbe47461c3bf37b
cebasypia <cebasypia.tskm8@gmail.com>
2020-05-17 00:44:27 +0900

    6 fixes Add authenticate_user! Add ensure_correct_user Set Book.new in #show & #index Set user_id in #create Add flash to view errors in #create Permit :body in book_params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,25 @@
 class BooksController < ApplicationController
+	before_action :authenticate_user!
+	before_action :ensure_correct_user, only: %i[edit update destroy]
 
   def show
-  	@book = Book.find(params[:id])
+		@book = Book.find(params[:id])
+		@new_book = Book.new
   end
 
   def index
-  	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+		@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+		@book = Book.new
   end
 
   def create
-  	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+		@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+		@book.user_id = current_user.id
+
   	if @book.save #入力されたデータをdbに保存する。
   		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
-  	else
+		else
+			flash.now[:danger] = "error"
   		@books = Book.all
   		render 'index'
   	end
@@ -27,8 +34,9 @@ class BooksController < ApplicationController
   def update
   	@book = Book.find(params[:id])
   	if @book.update(book_params)
-  		redirect_to @book, notice: "successfully updated book!"
-  	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
+			redirect_to @book, notice: "successfully updated book!"
+		else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
+			flash.now[:danger] = "error"
   		render "edit"
   	end
   end
@@ -42,7 +50,12 @@ class BooksController < ApplicationController
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title, :body)
   end
 
+	def ensure_correct_user
+		@book = Book.find(params[:id])
+		return if @book.user_id == current_user.id
+		redirect_to books_path, notice: '権限がありません'
+  end
 end
